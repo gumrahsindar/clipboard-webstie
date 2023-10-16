65a2c2e749bb2e84007c30c6d4167b9d75a95999
Gümrah Sindar <gsindar@gmail.com>
2023-10-16 11:34:20 +0300

    add prettier config

---
+++ .prettierrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: false,
+  trailingcomma: 'all',
+  singlequote: true,
+  arrowparens: 'always',
+  jsxsinglequote: false,
+  printwidth: 120,
+  plugins: ['prettier-plugin-tailwindcss'],
+}
